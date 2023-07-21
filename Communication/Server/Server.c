7f2748d07c57fdba1184601695e01bf8f20830c7
Hanifi Demir <101357112+hanifidemir5@users.noreply.github.com>
2023-07-21 15:50:52 +0300

    Update Server.c
    last touches

--- Communication/Server/Server.c
+++ Communication/Server/Server.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,12 +6,12 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
-#define PORT 8080
-#define MAX_CLIENTS 5
-#define BUFFER_SIZE 1024
-
-unsigned char key[BUFFER_SIZE];
-
+// Global variables
+#define PORT 8080 // PORT number which server will run can be different or can be taken from user
+#define MAX_CLIENTS 5 // It is what it say
+#define BUFFER_SIZE 1024 // Max byte length which server will process for one message
+unsigned char key[BUFFER_SIZE]; // Private key, can be declared every time server runs
+int clientCount = 0;
 
 // Structure to hold client information
 typedef struct {
@@ -20,136 +19,139 @@ typedef struct {
     char username[BUFFER_SIZE];
 } Client;
 
-// Global variables
-Client clients[MAX_CLIENTS];
-int clientCount = 0;
+Client clients[MAX_CLIENTS]; 
 
 // Function to handle communication with a client
-unsigned __stdcall ClientThread(void* arg) {
-    SOCKET clientSocket = *(SOCKET*)arg;
-    char buffer[BUFFER_SIZE] = { 0 };
-    char response[BUFFER_SIZE] = { 0 };
-    char message[BUFFER_SIZE + BUFFER_SIZE] = { 0 };
-    unsigned char senderUsernameLength;
+    unsigned __stdcall ClientThread(void* arg) 
+    {
+        SOCKET clientSocket = *(SOCKET*)arg;
+        char buffer[BUFFER_SIZE] = { 0 };
+        char response[BUFFER_SIZE] = { 0 };
+        char message[BUFFER_SIZE + BUFFER_SIZE] = { 0 };
+        unsigned char senderUsernameLength;
 
-    // Find the client's username
-    char* username = NULL;
-    for (int i = 0; i < clientCount; i++) {
-        if (clients[i].socket == clientSocket) {
-            username = clients[i].username;
-            break;
-        }
-    }
-
-    snprintf(key, BUFFER_SIZE, "%s", key);
-
-    if (send(clientSocket, key, strlen(key), 0) == SOCKET_ERROR) {
-        perror("send failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Send the list of connected users to the client
-    char userList[BUFFER_SIZE] = { 0 };
-    for (int i = 0; i < clientCount; i++) {
-        strncat(userList, clients[i].username, sizeof(userList) - strlen(userList) - 1);
-        strncat(userList, "\n", sizeof(userList) - strlen(userList) - 1);
-    }
-
-    send(clientSocket, userList, strlen(userList), 0);
-
-
-    while (1) {
-        // Receive client message
-        if (recv(clientSocket, buffer, BUFFER_SIZE, 0) == SOCKET_ERROR) {
-            perror("receive failed");
-            break;
-        }
-
-        printf("%s's message: %s and it's length is %d \n", username, buffer,strlen(buffer));
-
-        // Check if it is a private message
-        if (buffer[0] == '@') {
-            char* recipient = strtok(buffer + 1, " ");  // Skip the '@' symbol
-            char* messageText = strtok(NULL, "");       // Get the rest of the message
-
-            messageText[strlen(messageText)] = '\0';
-
-            unsigned char privateMessage[BUFFER_SIZE] = { 0 };
-            unsigned char privateUsername[BUFFER_SIZE] = { 0 };
-            unsigned char private[] = " (private)";
-            private[strlen(private)] = '\0';
-
-            // Get sender username
-            strcpy(privateUsername, username);
-            // add " (private)" at the end of the nick
-            strcat(privateUsername, private);
-            // null terminate username + " (private)" string
-            privateUsername[strlen(privateUsername)] = '\0';
-            // assign messageText in order to make it clear where accual message is 
-            strcpy(privateMessage, messageText);
-            // null terminate privateMessage
-            privateMessage[strlen(privateMessage)] = '\0';
-            // typecast privateUsernameLength to chat in order to send it in one byte 
-            senderUsernameLength = (char)strlen(privateUsername);
-
-            // Construct the private message
-            snprintf(message, sizeof(message), "%c%s%s", senderUsernameLength, privateUsername, privateMessage);
-
-
-            // Find the recipient in the client list
-            int recipientFound = 0;
-            for (int i = 0; i < clientCount; i++) {
-                if (strcmp(clients[i].username, recipient) == 0) {
-                    // Send the private message to the recipient
-                    send(clients[i].socket, message, strlen(message), 0);
-                    recipientFound = 1;
-                    break;
-                }
-            }
-
-            // If recipient not found, send an error message to the sender
-            if (!recipientFound) {
-                snprintf(response, BUFFER_SIZE, "Error: User '%s' not found.", recipient);
-                send(clientSocket, response, strlen(response), 0);
-            }
-        }
-        else 
+        // Find the client's username
+        char* username = NULL;
+        for (int i = 0; i < clientCount; i++) 
         {
-            senderUsernameLength = (char)strlen(username);
+            if (clients[i].socket == clientSocket) 
+            {
+                username = clients[i].username;
+                break;
+            }
+        }
 
-            // Construct the broadcast message
-            snprintf(message, sizeof(message), "%c%s%s", senderUsernameLength, username, buffer);
-            // Send the broadcast message to all clients except the sender
-            for (int i = 0; i < clientCount; i++) {
-                if (clients[i].socket != clientSocket) {
-                    send(clients[i].socket, message, strlen(message), 0);
+        // Construct key 
+        snprintf(key, BUFFER_SIZE, "%s", key);
+        // Send key to user to use for all messages
+        if (send(clientSocket, key, strlen(key), 0) == SOCKET_ERROR) 
+        {
+            perror("send failed");
+            exit(EXIT_FAILURE);
+        }
+        // Send the list of connected users to the client
+        char userList[BUFFER_SIZE] = { 0 };
+        for (int i = 0; i < clientCount; i++) 
+        {
+            strncat(userList, clients[i].username, sizeof(userList) - strlen(userList) - 1);
+            strncat(userList, "\n", sizeof(userList) - strlen(userList) - 1);
+        }
+        send(clientSocket, userList, strlen(userList), 0);
+
+
+        while (1) 
+        {
+            // Receive client message
+            if (recv(clientSocket, buffer, BUFFER_SIZE, 0) == SOCKET_ERROR) 
+            {
+                perror("receive failed");
+                break;
+            }
+            // The message which server takes
+            printf("%s's message: %s and it's length is %d \n", username, buffer,strlen(buffer));
+
+            // Check if it is a private message
+            if (buffer[0] == '@') 
+            {
+                char* recipient = strtok(buffer + 1, " ");  // Skip the '@' symbol
+                char* messageText = strtok(NULL, "");       // Get the rest of the message
+                unsigned char privateMessage[BUFFER_SIZE] = { 0 };
+                unsigned char privateUsername[BUFFER_SIZE] = { 0 };
+                unsigned char private[] = " (private)";
+                // Get sender username
+                strcpy(privateUsername, username);
+                // add " (private)" at the end of the nick 
+                strcat(privateUsername, private);
+                // assign messageText in order to make it clear where accual message is 
+                strcpy(privateMessage, messageText);
+                // typecast privateUsernameLength to chat in order to send it in one byte 
+                senderUsernameLength = (char)strlen(privateUsername);
+                // Construct private message
+                snprintf(message, sizeof(message), "%c%s%s", senderUsernameLength, privateUsername, privateMessage);
+                // Find the recipient in the client list
+                int recipientFound = 0;
+                for (int i = 0; i < clientCount; i++) 
+                {
+                    if (strcmp(clients[i].username, recipient) == 0) 
+                    {
+                        // Send the private message to the recipient
+                        send(clients[i].socket, message, strlen(message), 0);
+                        recipientFound = 1;
+                        break;
+                    }
+                }
+
+                // If recipient not found, send an error message to the sender
+                if (!recipientFound) 
+                {
+                    snprintf(response, BUFFER_SIZE, "Error: User '%s' not found.", username);
+                    send(clientSocket, response, strlen(response), 0);
                 }
             }
-        }
+            else 
+            {
+                // Find out sender username length as a char in order to sent it to client
+                senderUsernameLength = (char)strlen(username);
 
-        // Clear the buffers
-        memset(buffer, 0, BUFFER_SIZE);
-        memset(response, 0, BUFFER_SIZE);
-        memset(message, 0, BUFFER_SIZE);
-    }
-
-    // Remove the client from the client list
-    for (int i = 0; i < clientCount; i++) {
-        if (clients[i].socket == clientSocket) {
-            for (int j = i; j < clientCount - 1; j++) {
-                clients[j] = clients[j + 1];
+                // Construct the broadcast message
+                snprintf(message, sizeof(message), "%c%s%s", senderUsernameLength, username, buffer);
+                // Send the broadcast message to all clients except the sender
+                for (int i = 0; i < clientCount; i++) 
+                {
+                    if (clients[i].socket != clientSocket) 
+                    {
+                        send(clients[i].socket, message, strlen(message), 0);
+                    }
+                }
             }
-            clientCount--;
-            break;
+
+            // Clear the buffers
+            memset(buffer, 0, BUFFER_SIZE);
+            memset(response, 0, BUFFER_SIZE);
+            memset(message, 0, BUFFER_SIZE);
         }
+
+        // Remove the client from the client list
+        for (int i = 0; i < clientCount; i++) 
+        {
+            if (clients[i].socket == clientSocket) 
+            {
+                for (int j = i; j < clientCount - 1; j++) 
+                {
+                    clients[j] = clients[j + 1];
+                }
+                clientCount--;
+                break;
+            }
+        }
+        // Clear socket
+        closesocket(clientSocket);
+        _endthreadex(0);
+        return 0;
     }
 
-    closesocket(clientSocket);
-    _endthreadex(0);
-    return 0;
-}
-
-int main() {
+int main() 
+{
     WSADATA wsaData;
     SOCKET serverSocket, newSocket;
     struct sockaddr_in address;
@@ -157,28 +159,30 @@ int main() {
     HANDLE clientThreads[MAX_CLIENTS];
     unsigned threadID;
     int i, n;
-
+    // Take key from server for furter usage 
     while (1)
     {
         printf("Please enter your key(keys max lentgh must be 16): \n");
         fgets(key, BUFFER_SIZE, stdin);
-
-        // Remove the newline character from the message
+        // Remove the newline character from the key
         key[strcspn(key, "\n")] = '\0';
-
+        // Check if it is less or more than 16
         if (strlen(key) > 16)
         {
             printf("\nKey length must be less than 16!!\n");
             continue;
         }
-        else if (strlen(key) == 0) {
+        else if (strlen(key) == 0) 
+        {
             printf("\nYou must enter at least one character!!\n");
             continue;
         }
-        else if (strlen(key) == 16) {
+        else if (strlen(key) == 16) 
+        {
             break;
         }
-        else {
+        else 
+{
             for (i = strlen(key); i < 16; i++)
             {
                 key[i] = 0x01;
@@ -191,13 +195,15 @@ int main() {
     printf("key is %s and its length is : %d\n", key,strlen(key));
 
     // Initialize Winsock
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) 
+    {
         perror("WSAStartup failed");
         exit(EXIT_FAILURE);
     }
 
     // Create a socket
-    if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
+    if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) 
+    {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
@@ -208,29 +214,34 @@ int main() {
     address.sin_port = htons(PORT);
 
     // Bind the socket to the specified IP and port
-    if (bind(serverSocket, (struct sockaddr*)&address, sizeof(address)) == SOCKET_ERROR) {
+    if (bind(serverSocket, (struct sockaddr*)&address, sizeof(address)) == SOCKET_ERROR) 
+    {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
     // Listen for incoming connections
-    if (listen(serverSocket, 3) == SOCKET_ERROR) {
+    if (listen(serverSocket, 3) == SOCKET_ERROR) 
+    {
         perror("listen failed");
         exit(EXIT_FAILURE);
     }
 
     printf("Server listening on port %d\n", PORT);
 
-    while (1) {
+    while (1) 
+    {
         // Accept a new connection
-        if ((newSocket = accept(serverSocket, (struct sockaddr*)&address, &addrlen)) == INVALID_SOCKET) {
+        if ((newSocket = accept(serverSocket, (struct sockaddr*)&address, &addrlen)) == INVALID_SOCKET) 
+        {
             perror("accept failed");
             break;
         }
 
         // Receive the client's username
         char username[BUFFER_SIZE] = { 0 };
-        if (recv(newSocket, username, BUFFER_SIZE, 0) == SOCKET_ERROR) {
+        if (recv(newSocket, username, BUFFER_SIZE, 0) == SOCKET_ERROR) 
+        {
             perror("receive failed");
             break;
         }
@@ -242,7 +253,8 @@ int main() {
 
         // Create a thread for the new client
         clientThreads[clientCount - 1] = (HANDLE)_beginthreadex(NULL, 0, &ClientThread, (void*)&newSocket, 0, &threadID);
-        if (clientThreads[clientCount - 1] == NULL) {
+        if (clientThreads[clientCount - 1] == NULL) 
+        {
             perror("failed to create client thread");
             break;
         }
